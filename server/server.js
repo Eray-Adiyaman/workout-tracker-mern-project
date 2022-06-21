99fd8076f3e75efe37be2411f30b4f72c58e0d7a
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-06-21 13:33:16 +0300

    DBConnet,Mongoose,Model

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 const app = express();
@@ -15,8 +16,15 @@ app.use((req,res,next)=>{
 //routes
 app.use("/api/workouts",workoutRoutes)
 
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+        .then(()=>{
+            //listen
+            app.listen(process.env.PORT, ()=>{
+                console.log(`db connection succesfull & listening on ${process.env.PORT}!`)
+            })
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
 
-//listen
-app.listen(process.env.PORT, ()=>{
-    console.log(`listening on ${process.env.PORT}!`)
-})
