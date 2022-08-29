085cb500801fa71d02b12cc585055bace5a8bde5
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-08-29 15:59:56 +0300

    persist auth state throughout page refreshes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,29 +1,37 @@
-import { Link } from "react-router-dom"
-import { useLogout } from "../hooks/useLogout"
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 export default function Navbar() {
-    const { logout } = useLogout();
 
-    const handleClick = () => {
-        logout();
-    }
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    logout();
+  }
 
   return (
-        <header>
-            <div className="container">
-                <Link to="/">
-                    <h1>Workout Tracker</h1>
-                </Link>
-                <nav>
-                    <div>
-                        <button onClick={handleClick}>Logout</button>
-                    </div>
-                    <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Signup</Link>
-                    </div>
-                </nav>
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Tracker</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Logout</button>
             </div>
-        </header>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
   )
 }
