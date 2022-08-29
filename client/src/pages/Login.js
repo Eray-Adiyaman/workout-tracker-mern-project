aaa74458531fbd8e2cc421d09713083af92176a6
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-08-29 15:33:33 +0300

    Login hook

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,16 @@
 import { useState } from "react"
-
+import { useLogin } from "../hooks/useLogin";
 
 export default function Login (){
 
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const { login,error,isLoading } = useLogin();
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
 
-        console.log(email,password)
+        await login(email,password);
     }
     
     return(
@@ -29,7 +30,8 @@ export default function Login (){
                 value={password}
                 onChange={(e)=>setPassword(e.target.value)}
             />
-            <button>Login</button>
+            <button disabled={isLoading}>Login</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
